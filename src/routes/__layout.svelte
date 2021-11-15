afb2d0455c2947163735c40998194162f918bc0d
cliff <cliff.wong@ehlacademy.org>
2021-11-15 11:28:59 +0800

    __layout recovered

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,27 @@
 <script>
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+	import Modal from '../components/app-shelf/modal.svelte'
+	import Popper from '../components/app-shelf/popper.svelte'
+	import Notification from '../components/app-shelf/notification.svelte'
+	import TopBar from '../components/app-shelf/top-bar.svelte'
+	import {onMount} from "svelte";
+	import {getUserInfo} from "../api/user-api";
+	import {fetchTeacherList} from "../store/org-api";
+	import {fetchStudentList} from "../store/org-api";
+
+	export let segment;
+
+	onMount(() => {
+		getUserInfo()
+		fetchTeacherList()
+		fetchStudentList()
+	})
 </script>
 
-<Header />
-
-<main>
-	<slot />
-</main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
+<Notification>
+	<Popper>
+		<Modal>
+			<TopBar {segment}/>
+			<slot></slot>
+		</Modal>
+	</Popper>
+</Notification>
