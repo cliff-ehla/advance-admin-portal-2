3c8d2cbc423d94eff2e9add4095928c132580286
cliff <cliff.wong@ehlacademy.org>
2021-11-17 11:49:58 +0800

    added another day calendar

---
+++ src/routes/tutor/[user_id]/schedule.svelte
@@ -0,0 +1,6 @@
+<script>
+	import TutorWeekCalendar from '../../../components/calendar/tutor-week-calendar.svelte'
+	import {page} from '$app/stores'
+</script>
+
+<TutorWeekCalendar tutor_id={$page.params.user_id}/>
