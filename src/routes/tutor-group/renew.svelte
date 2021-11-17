900ff4f7601cc834433f8d573b5424290e17b837
cliff <cliff.wong@ehlacademy.org>
2021-11-17 11:50:32 +0800

    added tutor group

---
+++ src/routes/tutor-group/renew.svelte
@@ -0,0 +1,37 @@
+<script>
+	import {onMount} from 'svelte'
+	import {listNearlyCompleteTutorGroup} from "../../api/admin-api";
+
+	let list
+
+	onMount(async () => {
+		list = await listNearlyCompleteTutorGroup()
+	})
+</script>
+
+{#if list}
+	<table>
+		{#if list.length}
+			<tr>
+				<td>Student name</td>
+				<td>Course title</td>
+				<td>Complete count</td>
+				<td>Last lesson</td>
+			</tr>
+			{#each list as item}
+				<tr>
+					<td>{item.nicknames}</td>
+					<td>{item.title}</td>
+					<td>{item.actual_zoom_cnt} / {item.zoom_cnt}</td>
+					<td>{item.last_lesson}</td>
+				</tr>
+			{/each}
+		{/if}
+	</table>
+{/if}
+
+<style>
+    td {
+        padding: 1em;
+    }
+</style>
