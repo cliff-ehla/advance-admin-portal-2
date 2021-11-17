1c555998007329c5da2f4e63d8d597a88cc633d0
cliff <cliff.wong@ehlacademy.org>
2021-11-17 11:49:07 +0800

    added another day calendar

---
+++ src/routes/tutor/[user_id]/available-timetable.svelte
@@ -0,0 +1,17 @@
+<script>
+	import {onMount} from 'svelte'
+	import {fetchTutorAvailableTimeSlot} from "../../../api/tutor-api";
+	import TimeTableList from '../../../components/calendar/available-time-table-list.svelte'
+	import {page} from '$app/stores'
+	$: teacher_id = $page.params.user_id
+
+	let data
+
+	onMount(async () => {
+		data = await fetchTutorAvailableTimeSlot($page.params.user_id)
+	})
+</script>
+
+{#if data}
+	<TimeTableList time_slot_list={data} {teacher_id}/>
+{/if}
