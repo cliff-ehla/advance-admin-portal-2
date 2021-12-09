97688e5d395a31263ce18113fb296b9178be9415
cliff <cliff.wong@ehlacademy.org>
2021-12-09 12:18:42 +0800

    wip

---
+++ src/lib/option/confirmed-option-preview.svelte
@@ -0,0 +1,59 @@
+<script>
+	import dayjs from "dayjs";
+	import utc from "dayjs/plugin/utc.js";
+	dayjs.extend(utc)
+	import {tutor_store} from "../../store/tutor-store";
+	import Icon from "$lib/ui-elements/icon.svelte";
+	export let option
+	import OptionDraftDialog from '$lib/reservation/option-draft-dialog.svelte'
+	import CreateTrialDialog from '$lib/reservation/create-trial-dialog.svelte'
+	import {getContext} from 'svelte'
+	const {openModal} = getContext('simple-modal')
+
+	const openDraftDialog = () => {
+		openModal(OptionDraftDialog)
+	}
+
+	const openCreatDialog = () => {
+		openModal(CreateTrialDialog)
+	}
+</script>
+
+<div class="border border-gray-300 rounded-lg bg-white">
+	<div class="flex items-center justify-center mx-auto mt-4 mb-2">
+		<div on:click={() => {openDraftDialog(option)}} class="relative w-16 text-center overflow-hidden flex-shrink-0 p-1 rounded {!option.student_id ? 'border border-transparent hover:bg-blue-50 hover:shadow-lg hover:border-blue-300 cursor-pointer' : ''}">
+			<img src="{option.student_id ? '/student-f-icon.jpg' : option.s_nickname ? '/pre-user-icon.png' : '/phone-icon.png'}" alt="student"
+			     class="rounded-full w-12 h-12 mx-auto border border-gray-500">
+			<p class="font-bold text-center mt-1.5 text-xs whitespace-nowrap">
+				{#if option.student_id}
+					{option.student_nickname}
+				{:else}
+					{option.s_nickname || option.phone}
+					<button class="absolute top-0 right-0 cc bg-blue-500 text-white rounded-full w-6 h-6">
+						<Icon name="edit" className="w-3"/>
+					</button>
+				{/if}
+			</p>
+		</div>
+		<div class="ml-4 w-16 text-center overflow-hidden flex-shrink-0">
+			<img src="{tutor_store.getTutorProfilePic(option.reserves[0].teacher_id)}" alt="student"
+			     class="rounded-full w-12 h-12 mx-auto border border-gray-500">
+			<p class="font-bold text-center mt-1.5 text-xs whitespace-nowrap">
+				{tutor_store.getTutorName(option.reserves[0].teacher_id)}
+			</p>
+		</div>
+	</div>
+	<div class="px-2">
+		<div class="p-2 text-center leading-none border border-gray-300 rounded bg-blue-50 relative">
+			<p class="font-bold leading-none">{dayjs.utc(option.reserves[0].start_date).local().format('DD MMM (ddd)')}</p>
+			<p class="text-sm leading-none">{dayjs.utc(option.reserves[0].start_date).local().format('h:mma')} - {dayjs.utc(option.reserves[0].end_date).local().format('h:mma')}</p>
+			{#if option.student_id}
+				<button on:click={openCreatDialog} class="bg-blue-500 hover:bg-blue-700 text-white px-2 rounded absolute right-1 top-1 bottom-1">起堂</button>
+			{/if}
+		</div>
+	</div>
+	<div class="border-t border-gray-300 text-xs mt-2 bg-gray-50 rounded-b-lg px-2 flex items-center py-0.5">
+		<p>{option.remark}</p>
+		<div class="ml-auto"><button><Icon name="edit" className="w-2.5"/></button></div>
+	</div>
+</div>
--- src/routes/booking/option.svelte
+++ src/routes/booking/option.svelte
@@ -19,13 +19,13 @@
 	import {tutor_store} from "../../store/tutor-store";
 	import {dialog} from "$lib/store/dialog";
 	import Icon from "$lib/ui-elements/icon.svelte";
-	import OptionDraftDialog from '$lib/reservation/option-draft-dialog.svelte'
-	import {getContext} from 'svelte'
-	const {openModal} = getContext('simple-modal')
+	import ConfirmedOptionPreview from '$lib/option/confirmed-option-preview.svelte'
+
 
 	export let option_list
 	$: not_yet_locked_list = option_list.filter(item => !!item.auto_delete_date)
-	$: locked_list = option_list.filter(item => !item.auto_delete_date)
+	$: locked_list = option_list.filter(item => !item.auto_delete_date && !item.student_id)
+	$: ready_list = option_list.filter(item => !!item.student_id)
 
 	const lockOption = (slot) => {
 		dialog.confirm({
@@ -44,10 +44,6 @@
 				option_list = data
 			}
 		})
-	}
-
-	const openDraftDialog = (option) => {
-		openModal(OptionDraftDialog)
 	}
 </script>
 
@@ -83,7 +79,7 @@
 											<p class="text-sm leading-none">{dayjs.utc(slot.start_date).local().format('h:mma')} - {dayjs.utc(slot.end_date).local().format('h:mma')}</p>
 										</div>
 										<div class="ml-auto">
-											<button on:click={() => lockOption(slot)} className="button-secondary w-12">確定</button>
+											<button on:click={() => lockOption(slot)} class="button-secondary w-12">確定</button>
 										</div>
 									</div>
 								{/each}
@@ -97,47 +93,19 @@
 				{/each}
 			</div>
 		</div>
-		<div class="flex-1 ml-4">
+		<div class="w-80 ml-4">
 			<p class="font-bold mb-4">確定的時段</p>
-			<div class="grid gap-4 grid-cols-2 w-full">
+			<div class="grid gap-4">
 				{#each locked_list as option}
-					<div class="border border-gray-300 rounded-lg bg-white px-2 pb-2 pt-4">
-						<div class="flex items-center justify-center mx-auto mb-2">
-							<div on:click={() => {openDraftDialog(option)}} class="relative w-16 text-center overflow-hidden flex-shrink-0 p-1 rounded {!option.student_id ? 'border border-transparent hover:bg-blue-50 hover:shadow-lg hover:border-blue-300 cursor-pointer' : ''}">
-								<img src="{option.student_id ? '/student-f-icon.jpg' : option.s_nickname ? '/pre-user-icon.png' : '/phone-icon.png'}" alt="student"
-								     class="rounded-full w-12 h-12 mx-auto border border-gray-500">
-								<p class="font-bold text-center mt-1.5 text-xs whitespace-nowrap">
-									{#if option.student_id}
-										{option.student_nickname}
-									{:else}
-										{option.s_nickname || option.phone}
-										<button class="absolute top-0 right-0 cc bg-blue-500 text-white rounded-full w-6 h-6">
-											<Icon name="edit" className="w-3"/>
-										</button>
-									{/if}
-								</p>
-							</div>
-							<div class="ml-4 w-16 text-center overflow-hidden flex-shrink-0">
-								<img src="{tutor_store.getTutorProfilePic(option.reserves[0].teacher_id)}" alt="student"
-								     class="rounded-full w-12 h-12 mx-auto border border-gray-500">
-								<p class="font-bold text-center mt-1.5 text-xs whitespace-nowrap">
-									{tutor_store.getTutorName(option.reserves[0].teacher_id)}
-								</p>
-							</div>
-						</div>
-						<div>
-							<div class="p-2 text-center leading-none border border-gray-300 rounded bg-blue-50">
-								<p class="font-bold leading-none">{dayjs.utc(option.reserves[0].start_date).local().format('DD MMM (ddd)')}</p>
-								<p class="text-sm leading-none">{dayjs.utc(option.reserves[0].start_date).local().format('h:mma')} - {dayjs.utc(option.reserves[0].end_date).local().format('h:mma')}</p>
-							</div>
-							<div class="text-xs mt-2">
-								<p>備忘: {option.general_message || option.remark} <button class="button-secondary">修改</button></p>
-							</div>
-							{#if option.student_id}
-								<button class="button">Create {option.course}</button>
-							{/if}
-						</div>
-					</div>
+					<ConfirmedOptionPreview {option}/>
+				{/each}
+			</div>
+		</div>
+		<div class="w-80 ml-4">
+			<p class="font-bold mb-4">準備起堂</p>
+			<div class="grid gap-4">
+				{#each ready_list as option}
+					<ConfirmedOptionPreview {option}/>
 				{/each}
 			</div>
 		</div>
