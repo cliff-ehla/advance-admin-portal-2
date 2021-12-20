bfebae72e07e1d67da8e4b3f195e1a20727a5336
cliff <cliff.wong@ehlacademy.org>
2021-12-20 10:07:34 +0800

    student list done

--- src/routes/students/index.svelte
+++ src/routes/students/index.svelte
@@ -6,6 +6,7 @@
 	import Icon from '$lib/ui-elements/icon.svelte'
 	import CreateTickerDialog from '$lib/ticker/create-ticker-dialog.svelte'
 	import {student_store} from "../../store/student-store";
+	import Preview from '$lib/student/student-preview.svelte'
 
 	export let student_list = []
 
@@ -34,41 +35,7 @@
 
 <div class="p-4 bg-gray-50">
 	{#each $student_store as s}
-		<div class="flex items-center mb-4">
-			<a href="/students/{s.student_id}/tutor-group" class="flex items-center px-2 py-1 leading-tight rounded-full border border-gray-400 hover:border-blue-500 hover:bg-gray-200">
-				{#if s.gender === 'm'}
-					<img src="/student-m-icon.png" alt="student" class="w-8 h-8 rounded-full">
-				{:else if s.gender === 'f'}
-					<img src="/student-f-icon.jpg" alt="student" class="w-8 h-8 rounded-full">
-				{/if}
-				<div class="ml-2">
-					<p class="font-bold flex items-center">{s.student_nickname} <span class="text-xs bg-gray-100 border border-gray-300 px-0.5 ml-1 text-gray-500">{s.level}</span></p>
-					<p class="text-xs text-gray-500">@{s.student_username} <span class="ml-1">ID:{s.student_id}</span></p>
-				</div>
-			</a>
-			<div class="h-0.5 bg-gray-400 w-8"></div>
-			<div>
-				<div class="flex items-center leading-tight rounded-full border border-gray-400 hover:border-blue-500">
-					<div class="flex items-center px-2 py-1">
-						<img src="/parent-icon.png" alt="parent" class="w-8 h-8 rounded-full">
-						<div class="ml-1">
-							<p class="font-bold">{s.parent_nickname}</p>
-							<p class="text-xs text-gray-500">@{s.parent_username}</p>
-						</div>
-					</div>
-					<div class="px-2 py-1 ml-2 border-l border-gray-400">
-						<div class="flex items-center">
-							<img src="/ticket.jpg" alt="ticket" class="w-6 h-6 rounded-full">
-							<p class="ml-1">{s.r_t_amt}張</p>
-							<button on:click={() => {openCreateTickerDialog(s)}} class="text-blue-500 border-gray-400 hover:bg-blue-500 hover:text-white border p-1 rounded-full ml-1">
-								<Icon name="add" className="w-3"/>
-							</button>
-						</div>
-						<p class="text-xs text-gray-500 mt-0.5">已用: {s.used_t_amt}, 總共: {s.tt_t_amt}</p>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Preview {s}/>
 	{/each}
 </div>
 
