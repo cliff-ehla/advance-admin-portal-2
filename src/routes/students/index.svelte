75c29b4ec180aaf4c50f4293fe82d3d333759c7c
cliff <cliff.wong@ehlacademy.org>
2021-12-22 14:43:41 +0800

    fix

--- src/routes/students/index.svelte
+++ src/routes/students/index.svelte
@@ -4,7 +4,6 @@
 	const {openModal} = getContext('simple-modal')
 	import CreateUserDialog from '$lib/student/create-new-user-dialog.svelte'
 	import Icon from '$lib/ui-elements/icon.svelte'
-	import CreateTickerDialog from '$lib/ticker/create-ticker-dialog.svelte'
 	import {student_store} from "../../store/student-store";
 	import Preview from '$lib/student/student-preview.svelte'
 
@@ -18,17 +17,6 @@
 			}
 		}, {
 			padding: '1em'
-		})
-	}
-
-	const openCreateTickerDialog = (student) => {
-		openModal(CreateTickerDialog, {
-			parent_id: student.parent_id,
-			parent_nickname: student.parent_nickname,
-			onSuccess: ({ticket_amt}) => {
-				student.r_t_amt = Number(student.r_t_amt) + Number(ticket_amt)
-				student_list = student_list
-			}
 		})
 	}
 </script>
