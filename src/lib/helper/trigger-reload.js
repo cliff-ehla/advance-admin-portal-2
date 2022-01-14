b46942151de53f07313e18c99decdffec9188ae1
cliff <cliff.wong@ehlacademy.org>
2022-01-14 15:27:04 +0800

    misc

--- src/lib/helper/trigger-reload.js
+++ src/lib/helper/trigger-reload.js
@@ -1,7 +1,5 @@
 import {goto} from "$app/navigation";
 
 export const triggerReload = () => {
-	// console.log(get(page.path));
-	console.log('trigger reload:' + window.location.pathname)
 	goto(`${window.location.pathname}?reload=${new Date().getTime()}`)
 }
