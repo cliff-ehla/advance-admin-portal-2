fe3e46a1486d086e2e9bddfae8009f7041c0d648
cliff <cliff.wong@ehlacademy.org>
2022-02-24 18:26:20 +0800

    reduce zoom list all query range to avoid timeout

--- src/routes/api/zoomApi/zoom_list_all.js
+++ src/routes/api/zoomApi/zoom_list_all.js
@@ -2,8 +2,8 @@ import {usermodel} from "$lib/usermodal";
 
 export const post = (req) => {
 	return usermodel(req, '/zoomApi/zoom_list_all', {
-		start_time: req.body.start_time,
-		end_time: req.body.end_time,
+		start_date: req.body.start_time,
+		end_date: req.body.end_time,
 		teacher_ids: req.body.teacher_ids
 	})
 }
