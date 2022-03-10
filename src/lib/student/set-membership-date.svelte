a10bba72573f9a8d03415b114107244d7df29994
cliff <cliff.wong@ehlacademy.org>
2022-03-10 10:53:25 +0800

    set memerhsip date ui;

---
+++ src/lib/student/set-membership-date.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Datepicker from '$lib/ui-elements/date-picker/index.svelte'
+	import Button from '$lib/ui-elements/button.svelte'
+	import dayjs from "dayjs";
+	import {capitalize} from "$lib/helper/capitalize.js";
+
+	export let start_date
+	export let end_date
+	export let type
+	export let user_id
+	export let nickname
+	const onDateChange = (e) => {
+		start_date = dayjs(e.detail).format('YYYY-MM-DD')
+	}
+	const onDateChangeEnd = (e) => {
+		end_date = dayjs(e.detail).format('YYYY-MM-DD')
+	}
+	const onClick = () => {
+		console.log('cliff: ', start_date)
+	}
+</script>
+
+<p class="text-gray-600 mb-6">{capitalize(type)} membership for {nickname}</p>
+<div class="flex mt-4 mb-4">
+	<div>
+		<p class="text-sm text-gray-500">Start date</p>
+		<Datepicker selected={dayjs(start_date).toDate()} on:datechange={onDateChange}/>
+	</div>
+	<div class="ml-4">
+		<p class="text-sm text-gray-500">End date</p>
+		<Datepicker selected={dayjs(end_date).toDate()} on:datechange={onDateChangeEnd}/>
+	</div>
+</div>
+
+<div class="flex justify-end">
+	<Button on:click={onClick}>Confirm</Button>
+</div>
--- src/routes/students/membership.svelte
+++ src/routes/students/membership.svelte
@@ -14,20 +14,37 @@
 
 <script>
 	import dayjs from "dayjs";
+	import {getContext} from 'svelte'
+	const {openModal, closeModal} = getContext('simple-modal')
+	import SetMembershipDate from '$lib/student/set-membership-date.svelte'
 
 	export let list
-</script>
 
+	const onEdit = (user_id, nickname, type, obj) => {
+		console.log('cliff: ', user_id, type)
+		let start_date = obj ? obj.start_date : dayjs().format('YYYY-MM-DD')
+		let end_date = obj ? obj.end_date : dayjs().format('YYYY-MM-DD')
+		openModal(SetMembershipDate, {
+			type,
+			user_id,
+			nickname,
+			start_date,
+			end_date
+		}, {
+			overflow: 'initial'
+		})
+	}
+</script>
 <div class="container py-4">
 	{#each list as s}
 		<div class="p-4 flex items-center border-b border-gray-200">
 			<div class="mr-4">
 				<p>{s.nickname} <span class="px-1 leading py-0.5 text-xs text-gray-500 bg-gray-100 ml-1">{s.user_id}</span></p>
-				<p class="text-sm text-gray-500">{s.parent_nickname || 'NA'} <span class="px-1 leading py-0.5 text-xs text-gray-500 bg-gray-100 ml-1">{s.parent_id}</span></p>
+				<p class="text-sm text-gray-500">{s.parent_nickname || 'No name'} <span class="px-1 leading py-0.5 text-xs text-gray-500 bg-gray-100 ml-1">{s.parent_id}</span></p>
 			</div>
 			{#each ['vocab','dictation','speak'] as type}
-				<div class="flex mx-1">
-					<div class="{s[type] ? dayjs(s[type].end_date).isAfter(dayjs()) ? 'bg-blue-500 text-white' : 'bg-gray-100 border border-gray-300' : 'bg-gray-50 text-gray-300'} rounded p-2">
+				<div on:click={() => {onEdit(s.user_id, s.nickname, type, s[type])}} class="flex mx-1">
+					<div class="{s[type] ? dayjs(s[type].end_date).isAfter(dayjs()) ? 'bg-blue-500 text-white hover:bg-blue-400' : 'bg-gray-100 border border-gray-300 hover:bg-gray-200' : 'bg-gray-50 text-gray-300 hover:bg-gray-100 hover:text-gray-500'} rounded p-2 cursor-pointer">
 						<p class="text-xs">{type}</p>
 						{#if s[type]}
 							<p class="text-sm">{dayjs(s[type].start_date).format('DD MMM YYYY')} - {dayjs(s[type].end_date).format('DD MMM YYYY')}</p>
