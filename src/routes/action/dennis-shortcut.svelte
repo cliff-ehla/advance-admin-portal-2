09fb9152317dcf0f53ab49354b9c89118cedfd68
cliff <cliff.wong@ehlacademy.org>
2022-03-14 14:52:35 +0800

    dennis shortcut wip

--- src/routes/action/dennis-shortcut.svelte
+++ src/routes/action/dennis-shortcut.svelte
@@ -116,64 +116,64 @@
 		<input bind:value={parent_mobile} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
 	</div>
 	<div class="mt-4">
-		{#each childs as c, i}
-			<div class="bg-gray-100 p-2 py-4 rounded mb-2">
-				<div class="flex mb-2 justify-between">
-					<p class="text-blue-500 text-xs">#0{i + 1} Child</p>
-					{#if childs.length > 1}
-						<button class="hover:text-red-500" on:click={() => {onRemoveChild(i)}}>
-							<Icon name="close" className="w-3"/>
-						</button>
-					{/if}
-				</div>
-
-				<div>
-					<div class="text-sm text-gray-400 mb-2">
-						<p class="mb-1">Child nickname</p>
-						<input on:input={(e) => {onNicknameInput(e, c)}} bind:value={c.nickname} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
-					</div>
-					<div class="text-sm text-gray-400 mb-2">
-						<p class="mb-1">Gender</p>
-						<SelectionBox options={gender_options}
-						              selected_value={c.gender}
-						              on:input={e => {c.gender = e.detail}}/>
-					</div>
-					<div class="text-sm text-gray-400 mb-2">
-						<p class="mb-1">Grade</p>
-						<SelectionBox options={$level_options_store}
-						              selected_value={c.level}
-						              on:input={e => {c.level = e.detail}}/>
-					</div>
-					<div class="text-sm text-gray-400 mb-2">
-						<p class="mb-1">tutor_group_ids</p>
-						<input bind:value={c.tutor_group_ids} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+		<div class="grid grid-cols-2 gap-4 mb-4">
+			{#each childs as c, i}
+				<div class="border border-gray-300 p-2 py-4 rounded">
+					<div class="flex mb-2 justify-between">
+						<p class="text-blue-500 text-xs">#0{i + 1} Child</p>
+						{#if childs.length > 1}
+							<button class="hover:text-red-500" on:click={() => {onRemoveChild(i)}}>
+								<Icon name="close" className="w-3"/>
+							</button>
+						{/if}
 					</div>
 
-					<div class="text-sm text-gray-400 mb-2">
-						<div class="flex">
-							<p class="mb-1">Username</p>
-							<div class="ml-4">
-								{#if !!c.username}
-									{#if $is_loading}
-										loading...
-									{:else if !c.is_username_valid}
-										<p class="text-red-500">已有人用 :(</p>
-									{:else}
-										<p class="text-green-500">ok!</p>
-									{/if}
-								{/if}
-							</div>
+					<div>
+						<div class="text-sm text-gray-400 mb-2">
+							<p class="mb-1">Child nickname</p>
+							<input on:input={(e) => {onNicknameInput(e, c)}} bind:value={c.nickname} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
 						</div>
-						<input on:input={callApi} bind:value={c.username} type="text" class="border border-gray-200 rounded px-4 py-2 w-full bg-gray-100">
+						<div class="text-sm text-gray-400 mb-2">
+							<p class="mb-1">Gender</p>
+							<SelectionBox options={gender_options}
+							              selected_value={c.gender}
+							              on:input={e => {c.gender = e.detail}}/>
+						</div>
+						<div class="text-sm text-gray-400 mb-2">
+							<p class="mb-1">Grade</p>
+							<SelectionBox options={$level_options_store}
+							              selected_value={c.level}
+							              on:input={e => {c.level = e.detail}}/>
+						</div>
+						<div class="text-sm text-gray-400 mb-2">
+							<p class="mb-1">tutor_group_ids</p>
+							<input bind:value={c.tutor_group_ids} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+						</div>
+
+						<div class="text-sm text-gray-400 mb-2">
+							<div class="flex">
+								<p class="mb-1">Username</p>
+								<div class="ml-4">
+									{#if !!c.username}
+										{#if $is_loading}
+											loading...
+										{:else if !c.is_username_valid}
+											<p class="text-red-500">已有人用 :(</p>
+										{:else}
+											<p class="text-green-500">ok!</p>
+										{/if}
+									{/if}
+								</div>
+							</div>
+							<input on:input={callApi} bind:value={c.username} type="text" class="border border-gray-200 rounded px-4 py-2 w-full bg-gray-100">
+						</div>
 					</div>
 				</div>
-			</div>
-		{/each}
-		<div class="mb-2">
-			<button class="w-full text-center py-2 bg-gray-100 border border-gray-300 hover:bg-blue-500 hover:text-white" on:click={onAddChild}>Add child</button>
+			{/each}
+			<button class="h-full w-full text-center py-2 bg-gray-50 border border-gray-300 hover:bg-blue-50 hover:text-blue-500" on:click={onAddChild}>Add child</button>
 		</div>
 
-		<div class="bg-gray-100 p-2 py-4 rounded mb-2">
+		<div class="border border-gray-300 p-2 py-4 rounded mb-2">
 			<p class="text-blue-500 text-xs mb-2">Parent</p>
 			<div class="grid grid-cols-2 gap-4">
 				<div class="text-sm text-gray-400 mb-2">
