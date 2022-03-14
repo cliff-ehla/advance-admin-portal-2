8083cfe8e4b175a3b1da53722a944833c11a47ab
cliff <cliff.wong@ehlacademy.org>
2022-03-14 14:48:26 +0800

    dennis shortcut wip

--- src/routes/action/dennis-shortcut.svelte
+++ src/routes/action/dennis-shortcut.svelte
@@ -2,30 +2,37 @@
 	import dayjs from "dayjs";
 	import utc from 'dayjs/plugin/utc.js'
 	import {debounce} from 'debounce'
-	import {checkUsername} from "../../api/user-api";
 	import SelectionBox from '$lib/ui-elements/selection-box.svelte'
 	import Button from '$lib/ui-elements/button.svelte'
+	import Icon from '$lib/ui-elements/icon.svelte'
 	import {http} from "$lib/http";
 	import {createEventDispatcher} from 'svelte'
 	const dispatch = createEventDispatcher()
 	import {level_options_store} from "../../store/level-options";
 	import {student_store} from "../../store/student-store.js";
+	import {is_loading} from "$lib/store/is_loading";
+	import {capitalize} from "$lib/helper/capitalize.js";
+	import {notifications} from "$lib/store/notification.js";
 
 	dayjs.extend(utc)
 
-	let username = undefined
-	let nickname = undefined
-	let level = undefined
-	let gender = undefined
 	let parent_mobile = undefined
 	let parent_nickname = undefined
-	let is_vip_student = undefined
-	let PASSWORD = '12345678'
-
-	let lesson_fee, app_fee = 0, remark
-	let ticket_amt
-	let payment_method
-	let tutor_group_id
+	$: parent_username = parent_mobile
+	let is_parent_username_valid = false
+	const PASSWORD = '12345678'
+	const child_obj = {
+		username: '',
+		nickname: '',
+		password: PASSWORD,
+		level: '',
+		gender: '',
+		is_username_valid: false,
+		tutor_group_ids: ''
+	}
+	let childs = [
+		{...child_obj}
+	]
 
 	let gender_options = [
 		{
@@ -41,137 +48,159 @@
 			value: 'na'
 		}
 	]
-
-	let error
-	let child_username_exist
-	let parent_username_exist
-	let loading
-	let parent_username = parent_mobile
-	$: disabled = !(username && nickname && gender && level && parent_mobile && parent_nickname) || error
+	$: disabled = !(parent_mobile && parent_nickname)
 
 	const callApi = debounce(async () => {
-		loading = true
-		console.log('parent_username', parent_username)
-		let parent_success = checkUsername(parent_username)
-		let std_success = checkUsername(username)
-		let results = await Promise.all([parent_success, std_success])
-		parent_username_exist = !results[0]
-		child_username_exist = !results[1]
-		let success = results.every(r => !!r)
-		loading = false
-		error = !success
+		let promise_array = []
+		childs.forEach(c => {
+			promise_array.push(http.post(fetch,'/user/username_check',{
+				username: c.username
+			}))
+		})
+		let results = await Promise.all(promise_array)
+		results.forEach((res, i) => {
+			childs[i].is_username_valid = res.success
+			// childs[i].nickname = capitalize(childs[i].username)
+			// parent_nickname = capitalize(childs[i].username) + ' Parent'
+		})
+		childs = childs
+	}, 500)
+
+	const callApi2 = debounce(async () => {
+		const {success} = await http.post(fetch,'/user/username_check',{
+			username: parent_username
+		})
+		if (/^\d+$/.test(parent_username)) {
+			parent_mobile = parent_username
+		} else {
+			notifications.alert('家長username個practice係入電話')
+		}
+		is_parent_username_valid = success
 	}, 500)
 
 	const onConfirm = async  () => {
-		let {data} = await http.post(fetch, '/adminApi/create_parent_student', {
-			student_username: username,
-			student_nickname: nickname,
-			student_password: PASSWORD,
+		let {data} = await http.post(fetch, '/adminApi/create_parent_students', {
+			students: childs,
 			parent_username: parent_username,
 			parent_nickname,
 			parent_password: PASSWORD,
-			gender,
-			level,
 			parent_mobile
 		}, {
 			notification: '成功創建用戶'
 		})
 		dispatch('created', {
-			user_id: data.child.user_id,
-			gender,
-			level,
-			nickname,
-			username
+			childs: data.childs
 		})
 		await student_store.fetchData(fetch)
 	}
+
+	const onAddChild = () => {
+		childs.push({...child_obj})
+		childs = childs
+	}
+
+	const onRemoveChild = (i) => {
+		childs.splice(i, 1)
+		childs = childs
+	}
+
+	const onNicknameInput = (e, child) => {
+		child.username = e.target.value.toLowerCase().replace(' ', '') + (parent_mobile && parent_mobile.substring(0,4))
+		parent_nickname = e.target.value + ' Parent'
+	}
 </script>
 
-<div class="container py-4">
-	<div class="w-full">
-		<div class="mt-4">
-			<div class="grid grid-cols-2 gap-4">
-				<div class="text-sm text-gray-400 mb-2">
-					<div class="flex">
-						<p class="mb-1">Child username</p>
-						<div class="ml-4">
-							{#if loading}
-								loading...
-							{:else if child_username_exist}
-								<p class="text-red-500">username exist :(</p>
-							{:else if !!username}
-								<p class="text-green-500">ok!</p>
-							{/if}
-						</div>
-					</div>
-					<input bind:value={username} on:input={callApi} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+<div class="w-full container py-4">
+	<div class="text-sm text-gray-400 mb-2">
+		<p class="mb-1">Parent mobile no</p>
+		<input bind:value={parent_mobile} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+	</div>
+	<div class="mt-4">
+		{#each childs as c, i}
+			<div class="bg-gray-100 p-2 py-4 rounded mb-2">
+				<div class="flex mb-2 justify-between">
+					<p class="text-blue-500 text-xs">#0{i + 1} Child</p>
+					{#if childs.length > 1}
+						<button class="hover:text-red-500" on:click={() => {onRemoveChild(i)}}>
+							<Icon name="close" className="w-3"/>
+						</button>
+					{/if}
 				</div>
-				<div class="text-sm text-gray-400 mb-2">
-					<p class="mb-1">Child nickname</p>
-					<input bind:value={nickname} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+
+				<div>
+					<div class="text-sm text-gray-400 mb-2">
+						<p class="mb-1">Child nickname</p>
+						<input on:input={(e) => {onNicknameInput(e, c)}} bind:value={c.nickname} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+					</div>
+					<div class="text-sm text-gray-400 mb-2">
+						<p class="mb-1">Gender</p>
+						<SelectionBox options={gender_options}
+						              selected_value={c.gender}
+						              on:input={e => {c.gender = e.detail}}/>
+					</div>
+					<div class="text-sm text-gray-400 mb-2">
+						<p class="mb-1">Grade</p>
+						<SelectionBox options={$level_options_store}
+						              selected_value={c.level}
+						              on:input={e => {c.level = e.detail}}/>
+					</div>
+					<div class="text-sm text-gray-400 mb-2">
+						<p class="mb-1">tutor_group_ids</p>
+						<input bind:value={c.tutor_group_ids} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+					</div>
+
+					<div class="text-sm text-gray-400 mb-2">
+						<div class="flex">
+							<p class="mb-1">Username</p>
+							<div class="ml-4">
+								{#if !!c.username}
+									{#if $is_loading}
+										loading...
+									{:else if !c.is_username_valid}
+										<p class="text-red-500">已有人用 :(</p>
+									{:else}
+										<p class="text-green-500">ok!</p>
+									{/if}
+								{/if}
+							</div>
+						</div>
+						<input on:input={callApi} bind:value={c.username} type="text" class="border border-gray-200 rounded px-4 py-2 w-full bg-gray-100">
+					</div>
 				</div>
 			</div>
+		{/each}
+		<div class="mb-2">
+			<button class="w-full text-center py-2 bg-gray-100 border border-gray-300 hover:bg-blue-500 hover:text-white" on:click={onAddChild}>Add child</button>
+		</div>
 
+		<div class="bg-gray-100 p-2 py-4 rounded mb-2">
+			<p class="text-blue-500 text-xs mb-2">Parent</p>
 			<div class="grid grid-cols-2 gap-4">
 				<div class="text-sm text-gray-400 mb-2">
 					<div class="flex">
-						<p class="mb-1">Parent username</p>
+						<p class="mb-1">Username</p>
 						<div class="ml-4">
-							{#if loading}
-								loading...
-							{:else if parent_username_exist}
-								<p class="text-red-500">username exist :(</p>
-							{:else if !!parent_username}
-								<p class="text-green-500">ok!</p>
+							{#if !!parent_username}
+								{#if $is_loading}
+									loading...
+								{:else if !is_parent_username_valid}
+									<p class="text-red-500">已有人用 :(</p>
+								{:else}
+									<p class="text-green-500">ok!</p>
+								{/if}
 							{/if}
 						</div>
 					</div>
-					<input on:input={callApi} bind:value={parent_username} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+					<input on:input={callApi2} bind:value={parent_username} type="text" class="border border-gray-200 rounded px-4 py-2 w-full bg-gray-100">
 				</div>
 				<div class="text-sm text-gray-400 mb-2">
 					<p class="mb-1">Parent Nickname</p>
-					<input bind:value={parent_nickname} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
+					<input bind:value={parent_nickname} type="text" class="border border-gray-200 rounded px-4 py-2 w-full bg-gray-100">
 				</div>
-			</div>
-
-			<div class="grid grid-cols-2 gap-4">
-				<div class="text-sm text-gray-400 mb-2">
-					<p class="mb-1">Gender</p>
-					<SelectionBox options={gender_options} selected_value={gender} on:input={e => {gender = e.detail}}/>
-				</div>
-				<div class="text-sm text-gray-400 mb-2">
-					<p class="mb-1">Grade</p>
-					<SelectionBox options={$level_options_store} selected_value={level} on:input={e => {level = e.detail}}/>
-				</div>
-			</div>
-			<div class="text-sm text-gray-400 mb-2">
-				<p class="mb-1">Parent mobile no</p>
-				<input bind:value={parent_mobile} type="text" class="border border-gray-300 rounded px-4 py-2 w-full">
 			</div>
 		</div>
-
 	</div>
-
-	<label>Lesson fee</label>
-	<input class="input block mb-2 w-full" type="number" placeholder="Lesson fee" on:input={e => {ticket_amt = e.target.value}} bind:value={lesson_fee}/>
-	<label>App fee</label>
-	<input class="input block mb-2 w-full" type="number" placeholder="App fee" bind:value={app_fee}/>
-	<label>Payment method</label>
-	<SelectionBox simple_array options={['Alipay', 'Bank Transfer', 'Payme']} selected_value={payment_method} on:input={e => {payment_method = e.detail}}/>
-	<label>Remark</label>
-	<textarea class="input block mb-2 w-full" placeholder="Remark" bind:value={remark}/>
-	<div class="mb-2">
-		<input type="checkbox" bind:checked={is_vip_student} id="extra_option" class="cursor-pointer">
-		<label for="extra_option" class="cursor-pointer ml-2">是否特別關注學生</label>
-	</div>
-	<label>Tutor group id</label>
-	<input class="input block mb-2 w-full" type="number" placeholder="Tutor group id" bind:value={tutor_group_id}/>
-	<div class="mt-4">
-		<Button button_class="w-full py-2" disabled={disabled || loading} on:click={onConfirm}>建立用戶</Button>
+	<div>
+		<Button button_class="w-full py-2" disabled={disabled || $is_loading} on:click={onConfirm}>建立用戶</Button>
 	</div>
 </div>
-<style>
-	label {
-		@apply text-sm text-gray-400 mb-2;
-	}
-</style>
