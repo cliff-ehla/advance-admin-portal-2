5df09e6820cf7f53bd7dff1dce47f1d958ab4538
cliff <cliff.wong@ehlacademy.org>
2022-03-15 10:15:38 +0800

    add preview funciton

--- src/routes/syllabus/[syllabus_id].svelte
+++ src/routes/syllabus/[syllabus_id].svelte
@@ -50,8 +50,8 @@
 <div class="container py-4">
 	<div class="grid grid-cols-3 gap-4">
 		{#each syllabus as item}
-			<div class="cursor-pointer hover:border-blue-500 border-gray-100 border shadow rounded flex items-center relative group">
-				<button on:click={() => {onDelete(item.item_id)}} class="group-hover:opacity-100 opacity-0 absolute top-1 right-1 w-8 h-8 rounded-full hover:bg-gray-100 cc transition">
+			<div on:click={() => {open(`/item/${item.item_id}`,'Preview', "popup")}} class="cursor-pointer hover:border-blue-500 border-gray-100 border shadow rounded flex items-center relative group">
+				<button on:click|stopPropagation={() => {onDelete(item.item_id)}} class="group-hover:opacity-100 opacity-0 absolute top-1 right-1 w-8 h-8 rounded-full hover:bg-gray-100 cc transition">
 					<Icon name="trash" className="w-3 text-red-500"/>
 				</button>
 				<img src={item.thumbnail_path} class="w-20" alt={item.name}>
