3b3ceca715d8dd589dcb93fc417065da7721804c
cliff <cliff.wong@ehlacademy.org>
2022-03-21 11:35:03 +0800

    incomplete material

--- src/routes/item/incomplete.svelte
+++ src/routes/item/incomplete.svelte
@@ -20,14 +20,22 @@
 </script>
 
 <div class="container py-4">
-	{#each item_list as item}
-		<div class="border border-gray-300 p-4 rounded my-2">
-			<p>Item ID: {item.item_id}</p>
-			<p>Class code: {item.class_code}</p>
-			{#if item.c_description}
-				<p>{item.c_description}</p>
-			{/if}
-			<p>Created {dayjs(item.update_ts).fromNow()}</p>
-		</div>
-	{/each}
+	<h1 class="mb-4 text-xl">Incomplete items</h1>
+	<div class="grid grid-cols-3 gap-4">
+		{#each item_list as item}
+			<div class="border border-gray-300 rounded">
+				<div class="overflow-hidden relative" style="padding-top: 56%">
+					<img class="absolute inset-0" src={item.thumbnail_path} alt="hi">
+				</div>
+				<div class="p-4">
+					<p>{item.name}</p>
+					{#if item.start_date}
+						<p class="text-sm text-blue-500">Lesson start <b>{dayjs(item.start_date).fromNow()}</b></p>
+					{/if}
+					<p class="text-sm text-gray-500">Item ID: {item.item_id}</p>
+					<p class="text-sm text-gray-500">Class code: {item.class_code}</p>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
