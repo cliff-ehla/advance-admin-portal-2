1feeca4bc71ea3a498955a939319e8b00aa87b26
cliff <cliff.wong@ehlacademy.org>
2022-03-21 16:17:42 +0800

    batch create student in waiting list

--- src/lib/classroom/batch-create-classroom.svelte
+++ src/lib/classroom/batch-create-classroom.svelte
@@ -7,10 +7,15 @@
 	import {http} from "$lib/http.js";
 	import SelectionBox from '$lib/ui-elements/selection-box.svelte'
 	import DatePicker from '$lib/ui-elements/date-picker/index.svelte'
+	import Dropdown from '$lib/ui-elements/dropdown3.svelte'
+	import Icon from '$lib/ui-elements/icon.svelte'
+	import Checkbox from '$lib/ui-elements/checkbox.svelte'
 	import Button from '$lib/ui-elements/button.svelte'
 	import {triggerReload} from "$lib/helper/trigger-reload.js";
 	import {getContext} from 'svelte'
 	import {action_status} from "../../store/calendar-action-status-store.js";
+	import {capitalize} from "$lib/helper/capitalize.js";
+	import {tooltip} from "$lib/aciton/tooltip.js";
 
 	const {closeModal} = getContext('simple-modal')
 
@@ -29,6 +34,8 @@
 	let ticket = 98
 	let student_size = 4
 	let is_always_open = true
+	let waiting_list
+	let selected_users = []
 
 	const ticket_options = [
 		{
@@ -54,7 +61,10 @@
 			teacher_id
 		})
 		classroom_list = res.data
-		if (tutor_course_id) getTutorCourseDetail()
+		if (tutor_course_id) {
+			getTutorCourseDetail()
+			getTutorCourseWaitingList()
+		}
 	})
 
 	const getTutorCourseDetail = async () => {
@@ -74,6 +84,13 @@
 			student_size = last_classroom.student_size
 			is_always_open = last_classroom.is_always_open || true
 		}
+	}
+
+	const getTutorCourseWaitingList = async () => {
+		const {data, success} = await http.get(fetch, '/adminApi/show_waiting_list', {
+			tutor_course_id
+		})
+		if (success) waiting_list = data.length ? data[0].user_ids : null
 	}
 
 	$: {
@@ -103,10 +120,10 @@
 	const onClassroomSelected = e => {
 		tutor_course_id = e.detail
 		getTutorCourseDetail()
+		getTutorCourseWaitingList()
 	}
 
 	const onCreate = async () => {
-		console.log('is_always_open', is_always_open)
 		const course_data = computed_dates.map((date,i) => {
 			return {
 				item_id: selected_item_ids[i],
@@ -127,6 +144,18 @@
 		triggerReload()
 		closeModal()
 		action_status.set('')
+	}
+	
+	const onToggleStudent = (i, student) => {
+		if (!Array.isArray(selected_users[i])) {
+			selected_users[i] = []
+		}
+		if (selected_users[i].includes(student)) {
+			selected_users[i].splice(selected_users[i].indexOf(student), 1)
+		} else {
+			selected_users[i].push(student)
+		}
+		selected_users = selected_users
 	}
 </script>
 
@@ -208,6 +237,66 @@
 							<button on:click={() => {open(`/item/${selected_item_ids[i]}`,'Preview', "popup")}} class="button-secondary">Preview</button>
 						{/if}
 					</div>
+					<div class="w-8 ml-1 flex-shrink-0">
+						{#if waiting_list}
+							<Dropdown open_on_hover={false} activator_style="w-8 h-8 rounded-full cc bg-blue-50 text-blue-500" placement="bottom-end">
+								<button slot="activator">
+									<Icon name="avatar-add" className="w-5"/>
+								</button>
+								<div class="dropdown">
+									{#each waiting_list as student}
+										<div on:click|stopPropagation={() => {onToggleStudent(i,student)}} class="flex items-center my-0 cursor-pointer hover:bg-gray-100 px-2 py-2 rounded">
+											<div class="w-8 h-8 rounded-full border-1 border-gray-300 relative shadow flex-shrink-0">
+												<img src="/student-{student.gender}-icon.png" alt="gender" class="rounded-full border border-blue-500">
+												<div class="absolute shadow font-bold border border-white -bottom-1 -right-2 ml-2 w-5 h-5 bg-blue-500 rounded-full text-xs cc text-white">{capitalize(student.level)}</div>
+											</div>
+											<div class="ml-6">
+												<div class="flex items-center">
+													<p class="leading-none mb-0.5">{student.nickname}</p>
+												</div>
+												<p class="text-xs leading-none text-gray-500">{student.t_amt} ticket</p>
+											</div>
+											<div class="ml-4">
+												<Checkbox checked={selected_users[i] && selected_users[i].includes(student)}/>
+											</div>
+										</div>
+									{/each}
+								</div>
+							</Dropdown>
+						{:else}
+							<div use:tooltip={'waiting list is empty'}><Icon name="avatar-add" className="w-5 text-gray-300"/></div>
+						{/if}
+					</div>
+				{/if}
+			</div>
+			<div>
+				{#if selected_users[i] && selected_users[i].length}
+				{#each selected_users[i] as student}
+					<div class="inline-flex items-center mr-2 bg-gray-50 border border-blue-300 hover:border-blue-500 hover:bg-white rounded-full mt-2 relative">
+						<div class="w-8 h-8 rounded-full border-1 border-gray-300 relative shadow flex-shrink-0">
+							<img src="/student-{student.gender}-icon.png" alt="gender" class="rounded-full border border-blue-500">
+							<div class="absolute shadow font-bold border border-white -bottom-2 -right-4 ml-2 w-7 h-7 bg-blue-500 rounded-full text-sm cc text-white">{capitalize(student.level)}</div>
+						</div>
+						<div class="pl-5 pr-2 py-1">
+							<div class="text-sm whitespace-nowrap">{student.nickname}</div>
+							<div class="text-xs leading-none text-gray-500">@{student.username}</div>
+						</div>
+						<div class="pl-2 mr-4 py-1 border-l border-gray-400 text-xs leading-none text-gray-500" style="font-size: 10px;">
+							{#if student.total_t_amt <= 6 * 49}
+								<div class="bg-yellow-500 rounded text-white inline-block px-1 leading-tight mb-0.5">新用戶</div>
+							{:else if student.t_amt < 6 *49}
+								<div class="bg-red-500 rounded text-white inline-block px-1 leading-tight mb-0.5">要跟進大戶</div>
+							{:else}
+								<div class="bg-blue-500 rounded text-white inline-block px-1 leading-tight mb-0.5">大戶</div>
+							{/if}
+							<p>總共 ${student.total_t_amt}</p>
+							<p class="text-black mt-0.5">結餘 ${student.t_amt}</p>
+						</div>
+						<button on:click={() => {onToggleStudent(i, student)}} class="absolute -top-1.5 -right-2 bg-white cc rounded-full w-5 h-5 border border-blue-500 hover:text-red-500 hover:bg-red-50">
+							<Icon name="close" className="w-2"/>
+						</button>
+					</div>
+				{/each}
 				{/if}
 			</div>
 		{/each}
---
+++ src/lib/ui-elements/checkbox.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Icon from '$lib/ui-elements/icon.svelte'
+	export let checked
+</script>
+
+<div class="w-6 h-6 rounded cc border" class:unchecked={!checked} class:checked>
+	<Icon name="tick" className="w-3 fill-current"/>
+</div>
+
+<style>
+	.checked {
+		@apply bg-green-500 text-white;
+	}
+	.unchecked {
+		@apply bg-gray-200 text-white;
+	}
+</style>
