7878db13643a66228393672813cbf4fa6292f463
cliff <cliff.wong@ehlacademy.org>
2022-04-06 17:34:03 +0800

    again

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto'
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: cloudflare(),
 	}
 };
 
